a77f4771c4fd773eb76486ec8b0c6184ae828534
Aaron Carver <59704103+aaronmaxcarver@users.noreply.github.com>
2023-05-17 21:44:47 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [message, setMessage] = useState("before request");
+
+  async function getHelloWorld() {
+    try {
+      const response = await axios.get("http://localhost:3000/");
+      console.log(response.data);
+      return response.data;
+    } catch (error) {
+      console.error(error);
+      return "failed to fetch";
+    }
+  }
+
+  useEffect(() => {
+    const fetchStuff = async () => {
+      let new_message = await getHelloWorld();
+      setMessage(new_message);
+    };
+    fetchStuff();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{message}</p>
       </header>
     </div>
   );
